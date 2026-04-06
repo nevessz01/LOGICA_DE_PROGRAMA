3ea0f6e21a36d843bd434d01814ec3a2def3c530
jNeves tuffo 67 <jaomarcio79@gmail.com>
2026-04-06 20:04:51 -0300

    atividade

---
+++ Logica_de_Programação/06.04/aula04.js
@@ -0,0 +1,42 @@
+console.log("---- Operadores Aritméticos ----");
+var num1 = 4, num2 = 5; // Declaração de múltiplas variáveis
+
+// Operação de soma (+)
+var soma = num1 + num2;
+console.log("Soma: " + soma); 
+
+// Operação de subtração (-)
+var subtracao = num1 - num2;
+console.log("Subtração: " + subtracao);
+
+// Operação de multiplicação (*)
+var multiplicacao = num1 * num2;
+console.log("Multiplicação: " + multiplicacao);  
+
+// Operação de divisão (/)
+var divisao = num1 / num2;
+console.log("Divisão: " + divisao); 
+
+console.log("---- Operadores Relacionais ----"); 
+var nome = "João Marcio", nome2 = "joao marcio";
+
+//Operação de igualdade (==)
+var igual = nome == nome2; 
+console.log("igualdade: " + igual);
+
+//Operação de diferença (!=)
+var diferente = nome != nome2;
+console.log("Diferença: " + diferente);
+
+//Operação maior que (>)
+var maior = 2 > 1;
+console.log("Maior que: " + maior);
+
+//Operação de maior ou igual (>=)
+var maiorOuIgual = 2 >= 2;
+console.log("Maior ou igual: " + maiorOuIgual);
+
+//Operação de menor ou igual (<=)
+var maiorOuIgual = 2 <= 3;
+console.log("Menor ou igual: " + menorOuIgual);
+
