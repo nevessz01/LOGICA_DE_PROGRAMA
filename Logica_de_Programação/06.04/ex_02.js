9beb48f50830af7a1d99c14e412abce367f1e4ff
jNeves tuffo 67 <jaomarcio79@gmail.com>
2026-04-06 21:34:39 -0300

    atividade

---
+++ Logica_de_Programação/06.04/ex_02.js
@@ -0,0 +1,14 @@
+var nota1 = 6.7;
+var nota2 = 9.0;
+var media = (nota1 + nota2) / 2;
+
+console.log("Media: " + media);
+var operacao = nota1 > 7 && nota2 > 7;
+console.log("Passou: " + operacao);
+
+if (nota1 >= 7 && nota2 >= 7) {
+    console.log("Passou")
+}
+else{
+    console.log("Não passou")
+}
